7521fbfa3adf30e8f255abbdf57b26d7593e2c8e
sahar <you@example.com>
2024-01-26 15:06:18 +0330

    testable with local host

--- agrorob_emulator/entrypoint.sh
+++ agrorob_emulator/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/ash
-echo " * Starting: /zenoh-bridge-dds --connect e = tcp/57.129.16.234:8447 $*"
-exec /zenoh-bridge-dds --connect e = tcp/57.129.16.234:8447$*
+# echo " * Starting: /zenoh-bridge-dds --connect e = tcp/57.129.16.234:8447 $*"
+# exec /zenoh-bridge-dds --connect e = tcp/57.129.16.234:8447$*
+echo " * Starting: /zenoh-bridge-dds --connect e = tcp/localhost:8447 $*"
+exec /zenoh-bridge-dds --connect e = tcp/localhost:8447$*
