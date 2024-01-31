ee0adbca3ac154bbe9f52f14aaef38423eb74e7e
saharnn96 <you@example.com>
2024-01-31 12:35:01 +0330

    input run arguments added

--- agrorob_emulator/entrypoint.sh
+++ agrorob_emulator/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/ash
 # echo " * Starting: /zenoh-bridge-dds --connect e = tcp/57.129.16.234:8447 $*"
 # exec /zenoh-bridge-dds --connect e = tcp/57.129.16.234:8447$*
-echo " * Starting: /zenoh-bridge-dds --connect e = tcp/localhost:8447 $*"
-exec /zenoh-bridge-dds --connect e = tcp/localhost:8447$*
+echo " * Starting: /zenoh-bridge-dds -m client -e = tcp/localhost:8447 $*"
+exec /zenoh-bridge-dds -m client -e = tcp/localhost:8447$*
