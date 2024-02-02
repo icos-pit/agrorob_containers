a695c6f12db2efc094026676b1558e94c5942fe9
saharnn96 <you@example.com>
2024-02-02 15:09:24 +0330

    comment cleaning

--- agrorob_emulator/r_entrypoint.sh
+++ agrorob_emulator/r_entrypoint.sh
@@ -8,17 +8,6 @@ source "/workspaces/ws/install/setup.bash" --
 exec "$@"
 source "/workspaces/ws/install/local_setup.bash" --
 exec "$@"
-# echo "source /workspaces/ws/install/setup.bash$*"
-# echo "source /workspaces/ws/install/local_setup.bash$*"
-# echo "source /opt/ros/humble/setup.bash$*"
+
 ros2 bag play agrorob_server/bags/rosbag2_2023_11_30-09_27_02/rosbag2_2023_11_30-09_27_02_0.db3 --loop 
 exec "$@"
-
-# echo " * Starting: /zenoh-bridge-dds -m client -e = tcp/localhost:8447 $*"
-# exec /zenoh-bridge-dds -m client -e = tcp/localhost:8447$*
-# exec ros2 bag play agrorob_server/bags/rosbag2_2023_11_30-09_27_02/rosbag2_2023_11_30-09_27_02_0.db3 --loop $*
-# exec ros2 pkg list$*
-# source /workspaces/ws/install/setup.bash$*
-# source /workspaces/ws/install/local_setup.bash$*
-# source /opt/ros/humble/setup.bash$*
-# ros2 bag play agrorob_server/bags/rosbag2_2023_11_30-09_27_02/rosbag2_2023_11_30-09_27_02_0.db3 --loop 
