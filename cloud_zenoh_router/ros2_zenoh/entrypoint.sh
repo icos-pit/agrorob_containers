4ec9b1996b1599222ffe03b9231b135d89aee3d5
Sobczaczek <sobczakss111@gmail.com>
2024-08-29 10:39:41 +0200

    ros2 env fixed

--- cloud_zenoh_router/ros2_zenoh/entrypoint.sh
+++ cloud_zenoh_router/ros2_zenoh/entrypoint.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-echo "Sourcing ROS env: source /opt/ros/humble/setup.bash"
-
 # Source the ROS environment
+echo "Sourcing ROS env: source /opt/ros/humble/setup.bash"
 source /opt/ros/humble/setup.bash
 
-echo "Starting ROS2 talker demo: ros2 run demo_nodes_cpp talker &"
+set -e
+
+# setup ros2 environment
+source "/opt/ros/humble/setup.bash" --
+exec "$@"
+source "/workspaces/ws/install/setup.bash" --
+exec "$@"
+source "/workspaces/ws/install/local_setup.bash" --
+exec "$@"
+
+
+# echo "Starting ROS2 talker demo: ros2 run demo_nodes_cpp talker &"
 # Start ROS 2 talker node
 # ros2 run demo_nodes_cpp talker &
 
-echo "Starting ROS2 bridge: ros2 launch rosbridge_server rosbridge_websocket_launch.xml port:=8080 &"
 # Start the rosbridge server
+echo "Starting ROS2 bridge: ros2 launch rosbridge_server rosbridge_websocket_launch.xml port:=8080 &"
 ros2 launch rosbridge_server rosbridge_websocket_launch.xml port:=8080 &
 
-echo "Starting Zenoh Router with ros2 bridge: zenohd -c /zenoh/CUSTOM_CONFIG.json5 "$@""
 # Start the Zenoh daemon with the provided configuration file
+echo "Starting Zenoh Router with ros2 bridge: zenohd -c /zenoh/CUSTOM_CONFIG.json5 "$@""
 exec zenohd -c /zenoh/CUSTOM_CONFIG.json5 "$@"
